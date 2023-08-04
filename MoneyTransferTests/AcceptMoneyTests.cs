2ee9c8e917c6180158cf50e3ad8fdc72db80d35b
Megan Cindric <megancindric@gmail.com>
2023-08-04 16:01:11 -0600

    👌 IMPROVE: Completed AcceptMoney tests

--- MoneyTransferTests/AcceptMoneyTests.cs
+++ MoneyTransferTests/AcceptMoneyTests.cs
@@ -17,5 +17,31 @@ namespace MoneyTransferTests
             //Assert
             Assert.AreEqual(150,personOne.GetCashAmount());
         }
+
+        [TestMethod]
+        public void AcceptMoney_PersonStartsWith100_WalletMoneyDecreasedTo50()
+        {
+            //Arrange
+            Person personOne = new Person("Bingus", 100);
+
+            //Act
+            personOne.AcceptMoney(-50);
+
+            //Assert
+            Assert.AreEqual(50, personOne.GetCashAmount());
+        }
+
+        [TestMethod]
+        public void AcceptMoney_PersonStartsWith100_WalletMoneyStays100()
+        {
+            //Arrange
+            Person personOne = new Person("Bingus", 100);
+
+            //Act
+            personOne.AcceptMoney(0);
+
+            //Assert
+            Assert.AreEqual(100, personOne.GetCashAmount());
+        }
     }
 }
